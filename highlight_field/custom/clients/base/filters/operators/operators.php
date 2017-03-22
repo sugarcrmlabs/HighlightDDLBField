2cfb6cd9535144dd4aa90fcaad6280a5d7b03193
Molnar Szabolcs <szabolcs.molnar@softvision.ro>
2017-03-22 04:31:46 -0700

    Added operators for the custom field type so it shows up in the Search layout editor and it can be used in list view searches.

---
+++ highlight_field/custom/clients/base/filters/operators/operators.php
@@ -0,0 +1,10 @@
+<?php
+
+require('clients/base/filters/operators/operators.php');
+
+$viewdefs['base']['filter']['operators']['HighLightfield'] = array(
+    '$in' => 'LBL_OPERATOR_CONTAINS',
+    '$not_in' => 'LBL_OPERATOR_NOT_CONTAINS',
+    '$empty' => 'LBL_OPERATOR_EMPTY',
+    '$not_empty' => 'LBL_OPERATOR_NOT_EMPTY',
+);
